4ecfc105795748f218d83774d8999cf955c35bd2
Ariba Hussain <aribahussain970@gmail.com>
2024-11-28 22:23:00 +0500

    onboard class complete

--- lib/view/onboard_screen.dart
+++ lib/view/onboard_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery_app/model/onboard_model.dart';
 import 'package:flutter_food_delivery_app/utils/app_colors.dart';
@@ -19,7 +20,11 @@ class _OnboardScreenState extends State<OnboardScreen> {
         alignment: Alignment.topCenter,
         children: [
           PageView.builder(
-              onPageChanged: (value) {},
+              onPageChanged: (value) {
+                setState(() {
+                   currentIndex = value;
+                                   });
+              },
               itemCount: onboards.length,
               itemBuilder: (context, index) {
                 return Stack(
@@ -28,71 +33,111 @@ class _OnboardScreenState extends State<OnboardScreen> {
                       top: -70,
                       left: 0,
                       right: 0,
-                      child: Image.asset(
-                        onboards[index].image,
-                        width: 600,
-                        height: 600,
-                        fit: BoxFit.contain,
+                      child: FadeInDown(
+                        delay: Duration(milliseconds: 500),
+                        child: Image.asset(
+                          onboards[index].image,
+                          width: 600,
+                          height: 600,
+                          fit: BoxFit.contain,
+                        ),
                       ),
                     ),
                     Positioned(
                       top: MediaQuery.of(context).size.height / 1.9,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 25,
-                        ),
-                        child: Column(
-                          children: [
-                            Text(
-                              onboards[index].text1,
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 50,
-                                fontWeight: FontWeight.bold,
+                      child: FadeInUp(
+                        delay: Duration(milliseconds: 500),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 25,
+                          ),
+                          child: Column(
+                            children: [
+                              Text(
+                                onboards[index].text1,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 50,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            ),
-                            const SizedBox(
-                              height: 15,
-                            ),
-                            Text(
-                              onboards[index].text2,
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
+                              const SizedBox(
+                                height: 15,
                               ),
-                            ),
-                          ],
+                              Text(
+                                onboards[index].text2,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
                   ],
                 );
               }),
-          Positioned( 
-            bottom: 170, left: 25,
+          Positioned(
+            bottom: 170,
+            left: 25,
+            child: FadeInUp(
+              delay: Duration(milliseconds: 500),
               child: Row(
-            children: [
-              ...List.generate(
-                onboards.length,
-                (index) => AnimatedContainer(
-                  duration: const Duration(
-                    microseconds: 250,
+                children: [
+                  ...List.generate(
+                    onboards.length,
+                    (index) => AnimatedContainer(
+                      duration: const Duration(
+                        microseconds: 250,
+                      ),
+                      height: 5,
+                      width: 50,
+                      margin: const EdgeInsets.only(
+                        right: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        color: currentIndex == index
+                            ? Colors.white
+                            : Colors.white.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
                   ),
-                  height: 5,
-                  width: 50,
-                  margin: const EdgeInsets.only(
-                    right: 10,
-                  ),
-                  decoration: BoxDecoration(
-                    color: currentIndex == index ? Colors.white : Colors.white.withOpacity(0.5),
-                    
-                    borderRadius: BorderRadius.circular(15),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+              bottom: 30,
+              child: FadeInUp(
+                delay: Duration(milliseconds: 500),
+                child: SizedBox(
+                  height: 75,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                    ),
+                    child: MaterialButton(
+                      onPressed: () {},
+                      color: korange,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      minWidth: MediaQuery.of(context).size.width - 50,
+                      child: const Text(
+                        "Get Started",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ))
+              ))
         ],
       ),
     );
